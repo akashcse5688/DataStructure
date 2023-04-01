5983e500d5cae8bcbaff9f5361f205e098591895
akash <mdrakibulhasan878@gmail.com>
2023-04-01 19:33:05 +0600

    Basic funtion code

--- src/Function/Basic.java
+++ src/Function/Basic.java
@@ -1,5 +1,7 @@
 package Function;
 
+import java.util.Scanner;
+
 public class Basic {
     public static void Hellowrold(){
         System.out.println("Helow wrold");
@@ -7,9 +9,21 @@ public class Basic {
         System.out.println("Helow wrold");
         System.out.println("RAKIBUL HASAN AKASH");
     }
+    public static void Sum(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number=  ");
+        int a=sc.nextInt();
+        System.out.println("Enter the second number=  ");
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("Sum is = "+sum);
+    }
+
+
 
     public static void main(String[] args) {
         Hellowrold();
+        Sum();
 
     }
 }
