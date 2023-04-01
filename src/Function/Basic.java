e358b604aee9716bc9ff188153755f0ec320d95c
akash <mdrakibulhasan878@gmail.com>
2023-04-01 19:18:53 +0600

    Basic funtion code

---
+++ src/Function/Basic.java
@@ -0,0 +1,15 @@
+package Function;
+
+public class Basic {
+    public static void Hellowrold(){
+        System.out.println("Helow wrold");
+        System.out.println("Helow wrold");
+        System.out.println("Helow wrold");
+        System.out.println("RAKIBUL HASAN AKASH");
+    }
+
+    public static void main(String[] args) {
+        Hellowrold();
+
+    }
+}
