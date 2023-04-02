138c0bd3c1836a8825419f1b895657d53344f16e
akash <mdrakibulhasan878@gmail.com>
2023-04-02 16:31:50 +0600

    printarraynumber

---
+++ src/Array/Array2.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class Array2 {
+    public static void main(String[] args) {
+        int[]a={12,44,56,675};
+        for (int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+       }
+        for(int i:a){
+            System.out.println(i);
+        }
+    }
+
+    }
+
